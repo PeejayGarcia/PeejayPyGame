2b122528bc2fdf8e4aec31276172b2ae065ac0dd
Peejay Garcia <peejgarcia@gmail.com>
2019-05-20 23:47:03 -0500

    Created fundementals  elements dependencies.

--- Character/CharClasses.py
+++ Character/CharClasses.py
@@ -2,6 +2,8 @@
 def chibby(storyCharacter):
     strength = int
     speed = int
+    agility = int
+    hand-eye = int
     intelligence = int
     will = int
     spirit = int
@@ -14,3 +16,17 @@ def fundementals:
     ranged
     tactics
 
+def handtohand:
+    strength
+    speed
+    agility
+
+def grappling:
+    stength
+    speed
+    agility
+
+def blade:
+    hand-eye
+    speed
+    strength
